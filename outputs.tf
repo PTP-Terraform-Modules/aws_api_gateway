8d16158564bd99778daf6918ce1ab0149e2c985f
Micah Frederick <mfrederick@ptp.cloud>
2023-07-12 13:45:51 -0400

    Updates.

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "invoke_url" {
   value = aws_api_gateway_stage.rest_api_stage.invoke_url
 }
+
+output "id" {
+  value = aws_api_gateway_rest_api.rest_api.id
+}
+
+output "stage_name" {
+  value = aws_api_gateway_stage.rest_api_stage.stage_name
+}
